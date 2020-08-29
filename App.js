b8395e8fde7d96058ba0e5b28a650b03b267b916
Cassidy Pignatello <cass@Cassidys-MacBook-Air.local>
2020-08-29 17:56:04 +0700

    changes root component back to use Hooks

--- App.js
+++ App.js
@@ -1,58 +1,42 @@
 import { StatusBar } from 'expo-status-bar'
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, SafeAreaView, Platform } from 'react-native'
 import Constants from 'expo-constants'
 import TitleBar from './components/TitleBar'
 import SegmentedControls from './components/SegmentedControls'
 import EditablePatientList from './components/EditablePatientList'
 
-export default class App extends Component {
-  state = {
-    tabIndex: 0,
-    patients: [
-      'Yarik Nikolenko',
-      'Cassidy Pignatello',
-      'Zefanya Prabowo',
-      'Conor McGregor',
-      'Israel Adesanya',
-    ],
-  }
+export default function App() {
+  const [tabIndex, setTabIndex] = useState(0)
+  const handleTabsChange = (index) => setTabIndex(index)
 
-  handleTabsChange = (index) => {
-    this.setState({ tabIndex: index })
-  }
+  const [patients, setPatients] = useState([
+    'Yarik Nikolenko',
+    'Cassidy Pignatello',
+    'Zefanya Prabowo',
+    'Conor McGregor',
+    'Israel Adesanya',
+  ])
 
-  onAddPatient = (name) => {
-    const { patients } = this.state
+  const onAddPatient = (name) => setPatients([name, ...patients])
 
-    this.setState({
-      patients: [name, ...patients],
-    })
-  }
-
-  render() {
-    const { tabIndex, patients } = this.state
-    return (
-      <SafeAreaView style={styles.container}>
-        <StatusBar style='auto' />
-        <TitleBar title={'Book a Doctor'} />
-        <SegmentedControls
-          tabs={['Doctor', 'Video Consult']}
-          currentIndex={tabIndex}
-          onChange={this.handleTabsChange}
-          segmentedControlsBackgroundColor='#ebf5fe'
-          activeSegmentBackgroundColor='#fffdff'
-          activeTextColor='#8ac6c9'
-          textColor='#a0a7ae'
-          paddingVertical={10}
-        />
-        <EditablePatientList
-          patients={patients}
-          addPatient={this.onAddPatient}
-        />
-      </SafeAreaView>
-    )
-  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar style='auto' />
+      <TitleBar title={'Book a Doctor'} />
+      <SegmentedControls
+        tabs={['Doctor', 'Video Consult']}
+        currentIndex={tabIndex}
+        onChange={handleTabsChange}
+        segmentedControlsBackgroundColor='#ebf5fe'
+        activeSegmentBackgroundColor='#fffdff'
+        activeTextColor='#8ac6c9'
+        textColor='#a0a7ae'
+        paddingVertical={10}
+      />
+      <EditablePatientList patients={patients} addPatient={onAddPatient} />
+    </SafeAreaView>
+  )
 }
 
 const platformVersion =
